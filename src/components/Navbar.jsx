c104d3396e5d50f4dbf999d39c3324687f3056f4
mamczu <mateusz.mamczur@wp.pl>
2024-04-21 14:46:21 +0200

    Add footer to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,24 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className="bg-light-blue min-w-64 text-white flex flex-col items-center pt-4 min-h-svh">
-      <img src="src\assets\img\timer.svg" alt="Logo" className="mb-4 w-16 pt-4" />
-      <ul className='flex flex-col items-center gap-2'>
-        <li className="hover:bg-gray-200 hover:text-black p-2 rounded-md"><Link to="/">Wszystkie zadania</Link></li>
-        <li className="hover:bg-gray-200 hover:text-black p-2 rounded-md"><Link to="/current">Aktualne zadania</Link></li>
-        <li className="hover:bg-gray-200 hover:text-black p-2 rounded-md"><Link to="/priority">Priorytetowe zadania</Link></li>
-        <li className="hover:bg-gray-200 hover:text-black p-2 rounded-md"><Link to="/side">Poboczne zadania</Link></li>
-        <li className="hover:bg-gray-200 hover:text-black p-2 rounded-md"><Link to="/notes">Notatki</Link></li>
-      </ul>
+    <nav className="flex flex-col gap-4 items-center bg-light-blue min-w-64 text-white min-h-svh">
+      <header>
+        <img src="src\assets\img\timer.svg" alt="Logo" className="flexmb-4 w-16 pt-4" />
+      </header>
+      <main>
+        <ul className='flex flex-col items-center gap-2'>
+          <li className="hover:bg-gray-200 hover:text-black p-2 rounded-md"><Link to="/">Wszystkie zadania</Link></li>
+          <li className="hover:bg-gray-200 hover:text-black p-2 rounded-md"><Link to="/current">Aktualne zadania</Link></li>
+          <li className="hover:bg-gray-200 hover:text-black p-2 rounded-md"><Link to="/priority">Priorytetowe zadania</Link></li>
+          <li className="hover:bg-gray-200 hover:text-black p-2 rounded-md"><Link to="/side">Poboczne zadania</Link></li>
+          <li className="hover:bg-gray-200 hover:text-black p-2 rounded-md"><Link to="/notes">Notatki</Link></li>
+        </ul>
+      </main>
+      <footer className='flex items-center text-center p-4 border-t border-gray-300 mt-auto h-16 bottom-0'>
+        <span>
+          Wykonał Mateusz Mamczur
+        </span>
+      </footer>
     </nav>
   )
 }
