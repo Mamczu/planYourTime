9dba25779ccf784cec80d468a3cc550efeee41d1
mamczu <mateusz.mamczur@wp.pl>
2024-04-21 14:36:25 +0200

    Fix sidebar height

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className="bg-light-blue min-w-64 text-white h-full min-h-screen flex flex-col items-center pt-4">
+    <nav className="bg-light-blue min-w-64 text-white flex flex-col items-center pt-4 min-h-svh">
       <img src="src\assets\img\timer.svg" alt="Logo" className="mb-4 w-16 pt-4" />
       <ul className='flex flex-col items-center gap-2'>
         <li className="hover:bg-gray-200 hover:text-black p-2 rounded-md"><Link to="/">Wszystkie zadania</Link></li>
